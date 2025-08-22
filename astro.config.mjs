11611f6c88afac4deba76ba39facc14503db950d
singmickey <newaskj@gmail.com>
2025-08-22 15:14:37 +0800

    首次提交：初始化项目

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { remarkMermaid } from "./src/plugins/remark-mermaid.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
-	site: "https://singmickey.github.io",
+	site: "https://fgulify.netlify.app/",
 
 	base: "/",
 	trailingSlash: "always",
