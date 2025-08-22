e33978f95ea53a3dd246dec37cd4641059c58b05
singmickey <127318774+singmickey@users.noreply.github.com>
2025-08-22 16:33:59 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,9 +27,9 @@ import { remarkMermaid } from "./src/plugins/remark-mermaid.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
-	site: "https://singmickey.github.io",
+	site: "https://fusaq.netlify.app/",
 
-	base: "/ffiusmr",
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
